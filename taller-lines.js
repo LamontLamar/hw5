59633b6a9d46a557dc082e9098f3150864f4e11b
LamontLamar <43154878+LamontLamar@users.noreply.github.com>
2018-10-11 23:19:57 -0700

    Create taller-lines.js

---
+++ taller-lines.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(280);
+var a = 10;
+var b = 10
+
+while (a < height){
+line (a,0,a,b)
+var a = a + 10;
+var b = b+ 15;
+}
+}
