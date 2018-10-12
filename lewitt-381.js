de2ef15dabcfc0b816c581676c54bed1871ffc13
LamontLamar <43154878+LamontLamar@users.noreply.github.com>
2018-10-11 23:43:55 -0700

    Create lewitt-381.js

---
+++ lewitt-381.js
@@ -0,0 +1,20 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+function draw() {
+  background(220);
+  
+  rect(0, 0, 200, 200);
+	fill(20, 210, 210)
+	
+	rect(200, 200, 200, 200);
+	fill(255, 192, 210)
+	
+	rect(0, 200, 200, 200);
+	fill(189, 183, 107)
+
+  }
