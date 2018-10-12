c74c1f93342bf86d929314957c628b7af733e9e2
LamontLamar <43154878+LamontLamar@users.noreply.github.com>
2018-10-11 22:35:01 -0700

    Create concentric-circles.js

---
+++ concentric-circles.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(260);
+	
+var x = 5
+ while (x < height){
+noFill();
+ellipse (200,200,x,x)
+var x = x+20;
+ }
+
+}
